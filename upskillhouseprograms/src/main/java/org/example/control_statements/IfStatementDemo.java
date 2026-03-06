95070664d525a5cc93cd9a1277268f85b7933799
pparimi89 <pparimi89@gmail.com>
2026-03-06 17:11:36 +0530

    Operators and control statements

---
+++ upskillhouseprograms/src/main/java/org/example/control_statements/IfStatementDemo.java
@@ -0,0 +1,12 @@
+package org.example.control_statements;
+
+public class IfStatementDemo {
+    public static void main(String[] args) {
+        int carSpeed = 70;
+        if(carSpeed <= 80){
+            System.out.println("safe speed");
+        }
+
+    }
+
+}
---
+++ upskillhouseprograms/src/main/java/org/example/control_statements/If_else_StatementDemo.java
@@ -0,0 +1,14 @@
+package org.example.control_statements;
+
+public class If_else_StatementDemo {
+    public static void main(String[] args) {
+        int carSpeed = 85;
+        if(carSpeed <= 80){
+            System.out.println("safe speed");
+        }else{
+            System.out.println("Danger speed - He should be fined" );
+        }
+
+    }
+
+}
