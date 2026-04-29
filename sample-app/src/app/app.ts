f31ccb997abf9f9ba73babc13dd34f65e29a0e5a
naveen0913 <udayagirinaveen135@gmail.com>
2026-04-29 16:38:52 +0530

    added directives notes

--- sample-app/src/app/app.ts
+++ sample-app/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "../components/header/header";
+import { Home } from "../components/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
